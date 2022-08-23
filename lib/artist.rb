a8271e2bc5699017aa4cee9fedf5512918ce2f42
leroy-netizen <leroythomas051@gmail.com>
2022-08-23 14:26:26 +0300

    DONE

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,39 @@
 require 'pry'
-
+require_relative './concerns/memorable'
+require_relative './concerns/paramable'
+require_relative './concerns/findable'
 class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+    
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
+# binding.pry
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
+
+x = Artist.new
+x.name = "x"
+p Artist.find_by_name('x')
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,12 @@
 require 'pry'
-
+require_relative './concerns/memorable'
+require_relative './concerns/findable'
 class Song
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable::ClassMethods
+  extend Findable
 
   @@songs = []
 
@@ -10,20 +14,12 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
@@ -34,3 +30,7 @@ class Song
     name.downcase.gsub(' ', '-')
   end
 end
+
+y = Song.new
+y.name = "y"
+p Song.find_by_name('y')
